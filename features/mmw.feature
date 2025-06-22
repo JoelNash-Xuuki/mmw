cafb53dd20d2f9a67e43b384f8ca90eb80dd8386
Joel Nash <joelnash@xuuki.xyz>
2025-06-22 03:50:42 +0100

    update features tests

--- features/mmw.feature
+++ features/mmw.feature
@@ -5,6 +5,6 @@ Feature: Artist can render a midi and pdf file
 Scenario: Render a .ly file
   Given the file "/tmp/My-Project" does exist
   And the file "patches/testPatch-3" does exist
-  And Artist successfully runs `mmw -f "/tmp/My-Project" score "/tmp/My-Project/song.ly"`
+  And Artist successfully runs `mmw -n "/tmp/My-Project" score "/tmp/My-Project/song.ly"`
 
 
--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -2,8 +2,8 @@ Given('the file {string} does exist') do |string|
   expect(File.exist?(string)).to be true                                 
 end
 
-Given('Artist successfully runs `mmw -f {string} score {string}`') do |string, string2|
-  command = "bundle exec bin/mmw -f #{string} score #{string2}"
+Given('Artist successfully runs `mmw -n {string} score {string}`') do |string, string2|
+  command = "bundle exec bin/mmw -n #{string} score #{string2}"
   @output = `#{command}`                                                                                                                                                    
   expect($?.exitstatus).to eq(0)
   expect(@output).to include("Success: .ly file built")
