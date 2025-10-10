a15f0f27419398ade31eced436f79b8bdd128e25
Joel Nash <joel.nash@xuuki.xyz>
2025-10-10 12:17:39 +0100

    correct error for .ly build

--- features/mmw.feature
+++ features/mmw.feature
@@ -8,6 +8,6 @@ Scenario: Render a .ly file
   Given the file "/tmp/My-Project" doesn't exist
   Then the file "/tmp/My-Project" is created
   And the file "patches/testPatch-3" does exist
-  And Artist successfully runs `mmw -n "/tmp/My-Project" score "/tmp/My-Project/song.ly"`
+  And Artist successfully runs `mmw -n "/tmp/My-Project" score "--filename=/tmp/My-Project/song.ly"`
 
 
--- lib/score.rb
+++ lib/score.rb
@@ -1,18 +1,21 @@
 module Score
-  def build_score(global_options,options,args)
-    sheetName = options[:filename]
-    location = global_options[:n]
-   
-    command = "sheetbuilder #{sheetName} xuuki song-title vyvn patches/testPatch-3 test-song > /dev/null 2>&1 && rm #{sheetName}_* && mv #{sheetName}.ly #{location}"
+  def build_score(global_options, options, args)                                                                         
+   sheetName = options[:filename]                                                                                       
+   location = global_options[:n]                                                                                        
 
-    @output = `#{command}`
-
-    if $?.exitstatus == 0                                                                                             
-       puts "Success: .ly file built"                                                         
-    else                                                                                                              
-      raise RuntimeError, "Error: Score build failed."                                                            
-    end
-  end
+   command = "sheetbuilder #{sheetName} xuuki song-title vyvn patches/testPatch-3 test-song"                            
+                                                                                                                        
+   # Capture both stdout and stderr                                                                                     
+   @output = `#{command} 2>&1`                                                                                          
+                                                                                                                        
+   if $?.exitstatus == 0                                                                                                
+     puts "Success: .ly file built"                                                                                     
+   else                                                                                                                 
+     puts "Error: Score build failed."                                                                                  
+     puts "Details: #{@output}"  # Print the captured error output                                                      
+     raise RuntimeError, "Error: Score build failed."                                                                   
+   end                                                                                                                  
+ end  
 
   def render_score(global_options,options,args)
     lilypondFileName = options[:filename]
